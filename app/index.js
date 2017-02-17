54bbcd21a4a31c26053ac88baeb9cf1940f3b926
Jack Casey <Jack@jacks-air.home>
2017-02-17 12:23:57 +0000

    mongodb switch, views not being found

--- app/index.js
+++ app/index.js
@@ -1,12 +1,12 @@
 //'use strict'
-const path = require('path');
+require('import-export');
+const app = require('./server/app');
 const express = require('express');
+const path = require('path');
+const port = 3000;
 const exphbs = require('express-handlebars');
 const pg = require('pg')
 const bodyParser = require('body-parser');
-
-const app = express();
-const port = 3000;
 const conString = 'postgres://localhost:5432/nodedb';
 
 app.engine('.hbs', exphbs({
@@ -16,7 +16,7 @@ app.engine('.hbs', exphbs({
     partialsDir: path.join(__dirname, 'views/partials')
 }))
 app.set('view engine', '.hbs')
-app.set('views', path.join(__dirname,'views'))
+app.set('views',path.join(__dirname,'app/views'))
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -24,43 +24,24 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static(__dirname + '/src'))
 
-app.get('/', (req,res) => {
-	res.render('home', {
-		name: 'Jack'
-	})
-})
-
-app.get('/users', (req,res) => {
-	pg.connect(conString,(err,client,done) => {
-		if(err) return next(err)
-		client.query('SELECT name,age FROM users;',[],(err,result) => {
-			done()
-			if(err) return next(err)
-			res.render('users', {
-				rows: result.rows
-			})
-		})
-	})
-})
-
-app.post('/users', (req,res,next) => {
-	const user = req.body
-	console.log('USER CONSOLE LOG',user)
-	pg.connect(conString, (err,client,done) => {
-		if(err) return next(err)
-		client.query('INSERT INTO users (name, age) VALUES ($1, $2);',[user.name,user.age],(err,result) => {
-			done()
-			if(err) return next(err)
-			res.render('users')
-			//res.send(200)
-
-			console.log('user inserted into db');
-		})
-	})
-})
-
 app.listen(port, (err) => {
 	if(err) return console.log('CODE RED: MAINFRAME COMPROMISED',err);
 	console.log('CODE GREEN: MAINFRAME INITIALISED');
 	console.log(`MAINFRAME IS ACTIVE ON PORT ${port}`);
 })
+
+// app.post('/users', (req,res,next) => {
+// 	const user = req.body
+// 	console.log('USER CONSOLE LOG',user)
+// 	pg.connect(conString, (err,client,done) => {
+// 		if(err) return next(err)
+// 		client.query('INSERT INTO users (name, age) VALUES ($1, $2);',[user.name,user.age],(err,result) => {
+// 			done()
+// 			if(err) return next(err)
+// 			res.render('users')
+// 			//res.send(200)
+
+// 			console.log('user inserted into db');
+// 		})
+// 	})
+// })
